03d6b23e9918f2463f63e2548714ead55d34b626
Tarun yadav <tarunyadav9761@gmail.com>
2021-01-03 09:23:58 +0000

    modify na

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -4,56 +4,60 @@ import './product.css'
 
 const Product = (props) => {
 
-    const { id,name, rating,price,specs, imageUrl } = props.item
-    const { compareItem, setCompareItem} = props
+    const { id, name, rating, price, specs, imageUrl } = props.item
+    const { compareItem, setCompareItem } = props
 
-   
+
     const handleChange = (event) => {
-      const checkValue =  event.target.checked
+        const checkValue = event.target.checked
 
-      if(checkValue && compareItem.length < 4){
-          setCompareItem([...compareItem, props.item])
-      }
-      if(checkValue === false){
-          setCompareItem((prevState) => {
-           const state =   prevState.filter((item) => item.id !== id )
-           return state
-          })
-      }
+        if (checkValue && compareItem.length < 4) {
+            setCompareItem([...compareItem, props.item])
+        }
+
+        if (compareItem.length >= 4) {
+            alert("Only four items added in Compare List")
+        }
+        if (checkValue === false) {
+            setCompareItem((prevState) => {
+                const state = prevState.filter((item) => item.id !== id)
+                return state
+            })
+        }
     }
 
 
-    return(
+    return (
         <div className="container">
             <div className="image-container">
                 <img src={imageUrl} alt="mobile phone"></img>
                 <div className="compare-input">
                     <input onChange={handleChange} type='checkbox' id="compare" name="compare"></input>
-                    <label  for="compare">Add to Compare</label>
-                    
+                    <label for="compare">Add to Compare</label>
+
                 </div>
             </div>
             <div className='info-container'>
                 <div className='name'>
-                  <strong>
-                     {name}
-                      </strong>
-                         </div>
-                 <div className="rating">{rating}</div>
+                    <strong>
+                        {name}
+                    </strong>
+                </div>
+                <div className="rating">{rating}</div>
                 <div className="specs">
                     <ul>
-                       {
-                           specs.map((spec,index) => {
-                               return <li key={index}>{spec} </li>
-                           })
-                       }
+                        {
+                            specs.map((spec, index) => {
+                                return <li key={index}>{spec} </li>
+                            })
+                        }
                     </ul>
                 </div>
 
-                </div>
-                <div className="price-container">
-    <div>$ {price}</div>
-                </div>                
+            </div>
+            <div className="price-container">
+                <div>$ {price}</div>
+            </div>
         </div>
     )
 }
